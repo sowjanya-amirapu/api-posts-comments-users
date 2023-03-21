63c19a799066de57b43308213ae9aeb1fbd2a073
Sowjanya Amirapu <sowjanyaamirapu@Sowjanyas-MBP.lan>
2023-03-21 15:51:28 -0500

    final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState } from 'react';
+import './index.css';
+
 
 function App() {
+
+  const [buttonType, setButtonType] = useState('posts')
+  const [items, setItems] = useState([])
+
+  useEffect(()=>{
+
+    fetch(`https://jsonplaceholder.typicode.com/${buttonType}`)
+    .then(response => response.json())
+    .then(json => setItems(json))
+
+  }, [buttonType])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <button className='btn' onClick={() => setButtonType('posts')}>POSTS</button>
+    <button className='btn' onClick={() => setButtonType('users')}>USERS</button>
+    <button className='btn' onClick={() => setButtonType('comments')}>COMMENTS</button>
+    <p>{`Below are the list of all ${buttonType} that are called from the API
+    https://jsonplaceholder.typicode.com/${buttonType}`}</p>
+    <p>{buttonType}</p>
+    {items.map(item => {
+      return <pre key={item.id}>{JSON.stringify(item)}</pre>
+    })}
+    
     </div>
-  );
+  )
 }
 
 export default App;
